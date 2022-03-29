618dfd396ad5e6b0afe3625a2ae5255bdd9b6620
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-29 18:05:09 +0900

    ローディングとエラーの追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import { User } from "./types/api/user";
 import { UserProfile } from "./types/userProfile";
 
 export default function App() {
-  const [UserProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [UserProfiles, setUserProfiles] =
+  useState<Array<UserProfile>>([]);
+
+  const [loading,setLoding] = useState(false);
+  const [error,setError] = useState(false);
 
   const onClickFetchUser = () => {
+    setLoding(true);
+    setError(false);
+
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -18,16 +25,30 @@ export default function App() {
           email: user.email,
           address: `${user.address.city}${user.address.suite}${user.address.street}`
         }));
-        setUserProfiles(data)
+        setUserProfiles(data);
+      }).catch(()=>{
+        setError(true);
+      })
+      .finally(() => {
+        setLoding(false);
       });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color:"red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Lodding...</p>
+      ) : (
+        <>
       {UserProfiles.map((user)=>(
         <UserCard key={user.id} user={user} />
-      ))}
+        ))}
+        </>
+      )}
     </div>
   );
 };
