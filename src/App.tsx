82cb91cd8525f38792eb267efd12a67670fc4044
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-29 18:30:49 +0900

    hooksの作成、呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import axios from "axios";
-import { useState } from "react";
 import "./App.css";
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/userProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
+
 
 export default function App() {
-  const [UserProfiles, setUserProfiles] =
-  useState<Array<UserProfile>>([]);
 
-  const [loading,setLoding] = useState(false);
-  const [error,setError] = useState(false);
+  const {getUsers, userProfiles, loading, error} = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setLoding(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      }).catch(()=>{
-        setError(true);
-      })
-      .finally(() => {
-        setLoding(false);
-      });
-  };
+  const onClickFetchUser = () =>
+  getUsers();
 
   return (
     <div className="App">
@@ -44,7 +20,7 @@ export default function App() {
         <p>Lodding...</p>
       ) : (
         <>
-      {UserProfiles.map((user)=>(
+      {userProfiles.map((user)=>(
         <UserCard key={user.id} user={user} />
         ))}
         </>
